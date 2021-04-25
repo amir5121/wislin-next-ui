26b4eec00932a8b7dae7c2f02e9e19d84b1094b9
amirhosein <amirhoseinheshmati@gmail.com>
2021-04-26 00:00:52 +0430

    setting up new IDE

--- application/pages/index.js
+++ application/pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head"
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -8,13 +8,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>What should i learn next?</h1>
-
+      <main className="text-center">
+        <h1 className="text-9xl" style={{ transform: "rotate(-90deg)" }}>
+          S:
+        </h1>
+        <h1 className="text-xl">What should i learn next?</h1>
+        <a href="/users/auth/google/" className="btn--primary">
+          Log in With Google
+        </a>
         <p>
           Have you ever wondered what's best to learn to advance in your career?
         </p>
       </main>
     </div>
-  )
+  );
 }
